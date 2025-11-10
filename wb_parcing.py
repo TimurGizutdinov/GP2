29d240c0abadd7708a59809a39c11973e165c40b
AndreevArtemAndreevich <aranandreev@edu.hse.ru>
2025-11-11 01:09:55 +0300

    login to the website has been completed

--- wb_parcing.py
+++ wb_parcing.py
@@ -15,5 +15,12 @@ try:
     password_input = wait.until(EC.presence_of_element_located((By.ID, 'user_password')))
     submit_btn = wait.until(EC.presence_of_element_located((By.NAME, 'commit')))
     print('найдены поля для ввода логина и пароля и кнопки входа')
+
+    login_input.send_keys('mariya.strong@gmail.com')
+    password_input.send_keys('Mariya5')
+    print('ввод логина и пароля')
+
+    submit_btn.click()
+    print('тап по кнопке входа')
 finally:
     driver.quit()
